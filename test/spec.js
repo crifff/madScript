c5807d08b3ce36b4f9b3eef77ab1f003549957ba
hoshina <k-hoshina@nbsi.jp>
2013-04-25 18:23:35 +0900

    initial commit

---
+++ test/spec.js
@@ -0,0 +1,12 @@
+describe('audio', function () {
+    var $audio;
+
+    beforeEach(function () {
+        $audio = document.createElement('audio');
+    });
+
+    it('audio srcを読み込む', function () {
+        expect(true);
+
+    });
+});
